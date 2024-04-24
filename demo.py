66148f667dfb3aa604ba0c3ec0c6f010e769b694
sarah fullmer <sarah.fullmer@gmail.com>
2024-04-24 08:42:02 +0200

    remove non working open meteo stuff

--- demo.py
+++ demo.py
@@ -3,14 +3,9 @@
 import requests
 import json
 import matplotlib.pyplot as plt
-import openmeteo_requests
-
-import requests_cache
-import pandas as pd
 from retry_requests import retry
 
 r = requests.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m', verify=False)
-#print(r.json())
 response_json = r.json()
 #for key in response_json:
 #    print(key)
@@ -20,26 +15,4 @@ temperature_2m = response_json["hourly"]["temperature_2m"]
 print(len(time))
 print(len(temperature_2m))
 plt.plot(time,temperature_2m)
-#plt.show()
-
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
-
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-	"latitude": 52.52,
-	"longitude": 13.41,
-	"hourly": "temperature_2m"
-}
-responses = openmeteo.weather_api(url, params=params)
-
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+plt.show()
